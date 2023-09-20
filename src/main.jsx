fa7463ce73584c43d5bcd4ec86067cd80978a43b
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-09-20 16:27:42 -0500

    Main route creation and error handling

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
