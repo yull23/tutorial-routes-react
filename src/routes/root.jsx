6ae369bbce6bd7677eba53e24648e48436ef8ba7
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-09-20 18:24:16 -0500

    Adding Link using

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Root() {
   return (
     <>
       <div id="sidebar">
+        <nav>
+          <ul>
+            <li>
+              <Link to={`contacts/1`}>Your Name</Link>
+            </li>
+            <li>
+              <Link to={`contacts/2`}>Your Friend</Link>
+            </li>
+          </ul>
+        </nav>
         <h1>React Router Contacts</h1>
         <div>
           <form id="search-form" role="search">
