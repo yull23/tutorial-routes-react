44fd53fe34e3fb0da20d8b9dc003d150ec5b6f34
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-09-26 06:21:31 -0500

    Adding functionality for the edit route cancel button, with useNavigate()

--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -1,4 +1,4 @@
-import { Form, redirect, useLoaderData } from "react-router-dom";
+import { Form, redirect, useLoaderData, useNavigate } from "react-router-dom";
 import { updateContact } from "../contacts";
 
 export async function action({ request, params }) {
@@ -13,6 +13,7 @@ export async function action({ request, params }) {
 
 export default function EditContact() {
   const { contact } = useLoaderData();
+  const navigation = useNavigate();
 
   return (
     <Form method="post" id="contact-form">
@@ -58,7 +59,9 @@ export default function EditContact() {
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button type="button" onClick={() => navigation(-1)}>
+          Cancel
+        </button>
       </p>
     </Form>
   );
